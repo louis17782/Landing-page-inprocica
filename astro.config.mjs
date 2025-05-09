bdc375d382e5360c10194d63798b974d4aa5b3e3
louis17782 <luissanteliz22@gmail.com>
2025-05-09 12:14:10 -0400

    configure url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321/",
+  site: "https://inprocicainternational.netlify.app/",
   prefetch: true,
   vite: {
     css: {
